36b1273fb21d2d29a2e25558494e8c4b283f44f6
srujan0404 <srujan.23bcs10107@ms.sst.scaler.com>
2024-08-18 23:52:36 +0530

    changed some

--- src/components/common/footer/Footer.jsx
+++ src/components/common/footer/Footer.jsx
@@ -1,54 +1,49 @@
-import React from "react"
-import { footer } from "../../data/Data"
-import "./footer.css"
+import React, { useState } from "react";
+import "./header.css";
+import { nav } from "../../data/Data";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
+  const [navList, setNavList] = useState(false);
+
   return (
     <>
-      <section className='footerContact'>
-        <div className='container'>
-          <div className='send flex'>
-            <div className='text'>
-              <h1>Do You Have Questions ?</h1>
-              <p>We'll help you to grow your career and growth.</p>
-            </div>
-            <button className='btn5'>Contact Us Today</button>
+      <header>
+        <div className="container flex">
+          <div className="logo">
+            <img src="./images/logo.png" alt="" />
           </div>
-        </div>
-      </section>
-
-      <footer>
-        <div className='container'>
-          <div className='box'>
-            <div className='logo'>
-              <img src='../images/logo-light.png' alt='' />
-              <h2>Do You Need Help With Anything?</h2>
-              <p>Receive updates, hot deals, tutorials, discounts sent straignt in your inbox every month</p>
-
-              <div className='input flex'>
-                <input type='text' placeholder='Email Address' />
-                <button>Subscribe</button>
-              </div>
-            </div>
+          <div className="nav">
+            <ul className={navList ? "small" : "flex"}>
+              {nav.map((list, index) => (
+                <li key={index}>
+                  <Link to={list.path}>{list.text}</Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="button flex">
+            <h4>
+              <span>2</span> My List
+            </h4>
+            <button className="btn1">
+              <i className="fa fa-sign-out"></i> Sign In
+            </button>
           </div>
 
-          {footer.map((val) => (
-            <div className='box'>
-              <h3>{val.title}</h3>
-              <ul>
-                {val.text.map((items) => (
-                  <li> {items.list} </li>
-                ))}
-              </ul>
-            </div>
-          ))}
+          <div className="toggle">
+            <button onClick={() => setNavList(!navList)}>
+              {navList ? (
+                <i className="fa fa-times"></i>
+              ) : (
+                <i className="fa fa-bars"></i>
+              )}
+            </button>
+          </div>
         </div>
-      </footer>
-      <div className='legal'>
-        <span>© 2021 RentUP. Designd By GorkCoder.</span>
-      </div>
+      </header>
     </>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
